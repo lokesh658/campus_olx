355cfb96ae4ef9524d39756cee748f4134cb7769
lokesh sharma <85381561+lokesh658@users.noreply.github.com>
2022-06-27 15:21:34 +0530

    Update App_Header.js

--- src/components/App_Header.js
+++ src/components/App_Header.js
@@ -2,16 +2,6 @@ import { getByDisplayValue } from '@testing-library/react'
 import React, { Component } from 'react'
 
 class App_Header extends Component {
-    constructor(props) {
-        super(props)
-      
-        this.state = {
-           menu_arrow:true
-        }
-      }
-      menu_arrow_click=()=>{
-        this.state.menu_arrow ? (this.setState({menu_arrow:false})):this.setState({menu_arrow:true})
-      }
     render() {
         return (
             <div className="header">
@@ -20,8 +10,7 @@ class App_Header extends Component {
                     <input type="text" value='Find books,mobiles and many more...'/>
                     <div className="search_img"></div>
                 </div>
-                <div className='arrow_img' onClick={()=>this.menu_arrow_click} style={{display:this.state.menu_arrow?('block'):'none'}} ></div>
-                <div className="navbar" style={{flexFlow:'column',display:this.state.menu_arrow?("flex"):"flex"}}>
+                <div className="navbar">
                     <ul>
                     <li><div className="chat_icon"></div></li>
                     <li><div className="notify_icon"></div></li>
@@ -35,3 +24,4 @@ class App_Header extends Component {
 }
 
 export default App_Header
+
