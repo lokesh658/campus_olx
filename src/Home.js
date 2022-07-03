afc0e4f5c71312e78a0c307c1fa01c290bae5c46
Devid123349 <83571868+Devid123349@users.noreply.github.com>
2022-07-03 10:30:42 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react'
-import App_Header from './components/App_Header'
-import Footer from './components/Footer'
+import React from 'react'
+import AppHeader from './components/App_Header'
+import { Routes,Route } from 'react-router-dom';
+import {Home_page} from './components/Home_page.js';
+import { Edit } from './components/editview';
+import {Buyhistory} from './components/buyhistory';
+import {Sellhistory} from './components/sellhistory';
 
-export class Home extends Component {
-  render() {
+function Home (){
     return (
       <div>
-        <App_Header/>
-        <Footer/>
+        <AppHeader/>
+        <Routes>
+          <Route path='/' element={<Home_page/>}></Route>
+          <Route path='/editview' element={<Edit/>}></Route>
+          <Route path='/buyhistory' element={<Buyhistory/>}></Route>
+          <Route path='/sellhistory' element={<Sellhistory/>}></Route>
+        </Routes>
       </div>
     )
-  }
 }
 
-export default Home
+export default Home;
