1d91f71b0de0043bf330c50a8f5ea8cf27e4b7c4
Devid123349 <83571868+Devid123349@users.noreply.github.com>
2022-07-05 00:41:17 +0530

    Add files via upload

--- src/components/selling_items/aditemlist.js
+++ src/components/selling_items/aditemlist.js
@@ -4,25 +4,16 @@ import './itemlist.css'
 
 const Additemlist=({aditem})=>{
 
-    const Items=aditem.map( (items,i)=>{
-        return  <Sellitem key={i} 
-        id={aditem[i].id} 
-        amount={aditem[i].amount}
-        product_name={aditem[i].name} 
-        date={aditem[i].date}
-        />
-    })
-
     return(
         <div className="container-add">
-            {
-            aditem.map( (items,i)=>{
-        return  <Sellitem key={i} 
+         { 
+        aditem.map( (items,i)=>{
+        return ( <Sellitem key={aditem[i].id}    
         id={aditem[i].id} 
         amount={aditem[i].amount}
         product_name={aditem[i].name} 
         date={aditem[i].date}
-        />
+        />)
          })
           }
         </div>
