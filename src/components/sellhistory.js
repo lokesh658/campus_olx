7d578c69d96fb21cfadbf82e31e958a4ec33d316
Devid123349 <83571868+Devid123349@users.noreply.github.com>
2022-07-04 15:22:18 +0530

    Add files via upload

--- src/components/sellhistory.js
+++ src/components/sellhistory.js
@@ -1,17 +1,15 @@
 
-import a from './images/background.jpg'
+import Additemlist from './selling_items/aditemlist';
+import {Aditem} from './selling_items/aditems';
+
+/*import a from './images/background.jpg'
 import b from './images/bck.jpg'
 import c from './images/bg.jpg'
-import d from './images/ckg.jpg'
+import d from './images/ckg.jpg'*/
 import './sellhistory.css'
 
 export const Sellhistory = () => {
     return(
-        <div class="container-h">
-          <img alt="back" src={a}/>
-            <img alt="bck" src={b}/>
-            <img alt="bg" src={c}/>
-            <img alt="ckg" src={d}/>
-        </div>
+          <Additemlist aditem={Aditem} />
     )
 }
