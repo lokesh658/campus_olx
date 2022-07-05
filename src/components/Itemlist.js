ff7b50d4e126340273f7dcbcea045dd95c9dcd72
lokesh sharma <85381561+lokesh658@users.noreply.github.com>
2022-07-05 16:42:12 +0530

    Update Itemlist.js

--- src/components/Itemlist.js
+++ src/components/Itemlist.js
@@ -1,14 +1,52 @@
 import React from 'react'
 import {useState,useContext} from 'react'
 import { ItemContext } from '../Context'
+import AppHeader from './App_Header'
 import Sellitem from './sel-items'
+import { Items } from '../Context'
 
 function Itemlist() {
     const [items,setitems]=useContext(ItemContext)
+    const [range,setrange]=useState({lower:'',upper:''})
+    const filter_item=(item_category)=>{
+      const result=Items.filter((cur_data)=>{
+        return cur_data.category===item_category
+      })
+      setitems(result)
+    }
+    const filter_range=(lower,upper)=>{
+      const range_result=Items.filter((item_cat)=>{
+        return (item_cat.price>lower)&(item_cat.price<=upper)
+      })
+      setitems(range_result)
+    }
   return (
-    <div style={{display:'flex',flexFlow:'row wrap',margin:'40px calc(10%)',}}>{items.map((item)=>(
-      <Sellitem price={items.price} title={items.title} id={items.id} key={items.id}/>
-    ))}</div>
+    <div style={{display:'flex',flexFlow:'row wrap',margin:'40px calc(10%)',}}>
+      <div className="filter_list">
+        Fiter Items
+        <ul>
+          <li><button onClick={()=>filter_item('Book')}>Books</button></li>
+          <li><button onClick={()=>filter_item('Electronics')}>Electronics</button></li>
+          <li><button onClick={()=>filter_item('Cycle')}>Cycles</button></li>
+          <li><button onClick={()=>filter_item('Fashion')}>Fashion</button></li>
+          <li><button onClick={()=>filter_item('Sports')}>Sports</button></li>
+        </ul>
+        Filter Range
+        <ul>
+          <li><button onClick={()=>filter_range(0,200)}>below 200</button></li>
+          <li><button onClick={()=>filter_range(200,400)}>200-400</button></li>
+          <li><button onClick={()=>filter_range(400,600)}>400-600</button></li>
+          <li><button onClick={()=>filter_range(600,800)}>600-800</button></li>
+          <li><button onClick={()=>filter_range(800,100000)}>above 800</button></li>
+        </ul>
+        
+      </div>
+      <div>
+      {items.map((item)=>(
+      <Sellitem price={item.price} title={item.title} id={item.id} key={item.id}/>
+      ))}
+      </div>
+    </div>
   )
 }
 
